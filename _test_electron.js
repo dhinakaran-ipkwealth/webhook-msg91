1962d13e131d3819b39f8e44fc6255439e0fe0bf
dhinakaran-ipkwealth <dhinakaran@ipkwealth.com>
2026-07-06 16:30:30 +0530

    Remove old codes

--- _test_electron.js
+++
@@ -1,1 +0,0 @@
-const e = require("electron"); console.log(typeof e, Object.keys(e).join(",").slice(0,100)); if(e.app) e.app.whenReady().then(()=>process.exit(0)); else process.exit(1);
--- pngtoico.js
+++
@@ -1,7 +0,0 @@
-const pngToIco = require('png-to-ico');
-const fs = require('fs');
-
-(async () => {
-  const buf = await pngToIco('logo512.png');
-  fs.writeFileSync('webhook_msg91.ico', buf);
-})();
